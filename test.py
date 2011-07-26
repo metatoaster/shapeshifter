c91aa3fdfa0ada1c207f9a7c4c71b4bfef6760e7
Tommy Yu <y@metatoaster.com>
2011-07-27 03:14:41 +1200

    Version bump.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from os.path import join
 from shapeshifter.parser import Parser
-p = Parser(join('data', 'Shapeshifter.txt'))
+p = Parser(join('data', 'derp.txt'))
 p.parse()
 
 from pprint import pprint as pp
