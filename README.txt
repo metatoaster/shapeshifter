82bab8f54b7cca9e818e83ea48f1ac1a8d545a34
Tommy Yu <y@metatoaster.com>
2011-07-27 02:44:09 +1200

    Supporting setuptools.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A simple text entry to CSV converter; based on a file named shapeshifter.
+
+This also serve as a simple GUI app for Python.
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(
+    name='shapeshifter',
+    version=version,
+    description='Shapeshifter (?) Parser?',
+    long_description=open('README.txt').read() + "\n" +
+                     open(os.path.join('docs', 'HISTORY.txt')).read(),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+    ],
+    keywords='',
+    author='Tommy Yu',
+    author_email='y@metatoaster.com',
+    url='http://metatoaster.com/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['shapeshifter'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
