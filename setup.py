6dc0aa41042fa67657ea8c5ce27c730d10263ec3
Tommy Yu <y@metatoaster.com>
2011-09-07 01:26:44 +1200

    Moving readme into rst format.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='shapeshifter',
     version=version,
     description='Shapeshifter (?) Parser?',
-    long_description=open('README.txt').read() + "\n" +
+    long_description=open('README.rst').read() + "\n" +
                      open(os.path.join('docs', 'HISTORY.txt')).read(),
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
